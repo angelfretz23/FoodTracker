ca128c2dc41af6aaa90500e1cd8753f8370fe1e3
Angel Contreras <angel.contreras23@me.com>
2016-05-07 19:23:51 -0700

    Fix And Added
    Changed NSLabel and NSTextBox to UILabel and UITextField. Added and
    action.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,14 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 	// MARK: Properties
-	@IBOutlet weak var mealNameLabel: NSLabel!
-	@IBOutlet weak var mealText
+	@IBOutlet weak var mealNameLabel: UILabel!
+	@IBOutlet weak var nameTextField: UITextField!
+
+	
+	// MARK: Actions
+	@IBAction func setDefaultLabelText(sender: UIButton){
+		mealNameLabel.tet = "Default Text"
+	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+	
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
