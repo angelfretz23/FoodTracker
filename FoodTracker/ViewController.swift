085b6f56cdcbbe169daeff272d8d42f41493f292
Angel Contreras <angel.contreras23@me.com>
2016-05-07 19:01:01 -0700

    Added Properies
    Added outlets for mealTextBox and mealNameLabel.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+	// MARK: Properties
+	@IBOutlet weak var mealNameLabel: NSLabel!
+	@IBOutlet weak var mealText
 
     override func viewDidLoad() {
         super.viewDidLoad()
