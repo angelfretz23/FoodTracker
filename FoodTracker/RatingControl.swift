3981623048cce1a268fbf8302ae942cc648ba60e
Angel Contreras <angel.contreras23@me.com>
2016-05-23 22:49:18 -0700

    Check Point

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,18 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Angel Contreras on 5/23/16.
+//  Copyright © 2016 Angel Contreras. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+
+}
